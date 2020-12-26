fffbac4e632a66e99b9c13241fe7effde8cd4899
Thestickman391 <stickman391@gmail.com>
2020-12-26 22:05:22 +1300

    Init

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// var i = 180;
+// window.onload = function () {
+// document.getElementById('sunMoonCycle').style.rotate = (i % 360) + 'deg';
+// var myInterval = setInterval(function() {
+    // document.getElementById('sunMoonCycle').style.rotate = (i % 360) + 'deg';
+	// i += 1.8;
+// }, 80);
+// }
---
+++ weather.js
@@ -0,0 +1,31 @@
+var weather = require('openweather-apis');
+var geoip = require('geoip-lite');
+var ip = '115.189.87.37'
+var geo = geoip.lookup(ip);;
+console.log(geo.city)
+require('dotenv').config();
+
+weather.setLang('en');
+
+// or set the coordinates (latitude,longitude)
+weather.setCoordinate(geo.ll[0], geo.ll[1]);
+
+// 'metric'  'internal'  'imperial'
+weather.setUnits('metric');
+
+weather.setAPPID(process.env.API_KEY);
+
+weather.getTemperature(function(err, temp){
+	if(err) console.log(err);
+	console.log(temp);
+});
+
+weather.getDescription(function(err, desc){
+	if(err) console.log(err);
+	console.log(desc);
+});
+
+
+app.listen((process.env.PORT || 80), () =>
+  console.log('api listening on port 80!'),
+)
