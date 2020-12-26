dbb2adafae916c49c06a30317559e165af1220cd
Thestickman391 <stickman391@gmail.com>
2020-12-26 23:03:53 +1300

    added option to sync to current time

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
-// var i = 180;
-// window.onload = function () {
-// document.getElementById('sunMoonCycle').style.rotate = (i % 360) + 'deg';
-// var myInterval = setInterval(function() {
-    // document.getElementById('sunMoonCycle').style.rotate = (i % 360) + 'deg';
-	// i += 1.8;
-// }, 80);
-// }
+window.addEventListener('load', () => {
+	var button = document.getElementById("start");
+	button.onclick = function() {
+		var d = new Date();
+		console.log(d.getSeconds());
+		console.log(d.getHours());
+		console.log(d.getMinutes());
+		console.log(d.getMilliseconds());
+		totalSeconds = (d.getSeconds() + ((d.getHours() * 60) * 60) + (d.getMinutes() * 60) + (d.getMilliseconds() / 1000));
+		console.log(totalSeconds);
+		document.getElementById('skyGradient').children[0].children[0].setAttribute("begin", '-' + totalSeconds + 's');
+		document.getElementById('skyGradient').children[0].children[1].setAttribute("begin", '-' + totalSeconds + 's');
+		document.getElementById('skyGradient').children[1].children[0].setAttribute("begin", '-' + totalSeconds + 's');
+		
+		document.getElementById('skyGradient').children[0].children[0].setAttribute("dur", '86400s');
+		document.getElementById('skyGradient').children[0].children[1].setAttribute("dur", '86400s');
+		document.getElementById('skyGradient').children[1].children[0].setAttribute("dur", '86400s');
+		
+		document.getElementById("skyGradient").innerHTML += ""; //reload the svgs
+		
+		document.getElementById('sunMoonCycle').style.animationDuration = "86400s";
+		document.getElementById('sunMoonCycle').style.animationDelay = '-' + (totalSeconds + 43200) + 's';
+	}
+});
