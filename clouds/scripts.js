8981ae13b8ec774677609bb8c1262c5b4f796c26
Thestickman391 <stickman391@gmail.com>
2020-12-30 18:13:53 +1300

    Fixed issues with timing, removed hard-coded pixel values and changed various number ranges

--- clouds/scripts.js
+++ clouds/scripts.js
@@ -1,13 +1,14 @@
 window.onload = function() {
   for(var i = 0; i < 50; i++) {
-		var cloudSpeed = Math.floor(Math.random() * 8) + 5;
+		var cloudSpeed = Math.floor(Math.random() * 10) + 1;
+		console.log(cloudSpeed);
 		var div = document.createElement("div");
 		div.style.animationDuration = (15 * (1 + (cloudSpeed / 10))) + "s";
-		div.style.animationDelay = "-" + (((Math.floor(Math.random() * 100) + 1) / 100) * cloudSpeed)+ "s";
+		div.style.animationDelay = "-" + (((Math.floor(Math.random() * 100) + 1) / 100) * (15 * (1 + (cloudSpeed / 10))))+ "s";
 		div.style.transform = "scale(" + (cloudSpeed / 10) + ")";
 		div.style.opacity = (cloudSpeed / 10);
 		div.className = "cloud";
-		div.style.top = (Math.floor(Math.random() * 90) + 10) + "vh";
+		div.style.top = (Math.floor(Math.random() * 100) + 1) + "vh";
 		document.getElementById("clouds").appendChild(div);
     }
 }
