8c9de11e059f2de8ae117f7c9df041eaacff2b19
Thestickman391 <stickman391@gmail.com>
2021-01-11 21:50:34 +1300

    Swapped video for canvas to prevent issues where the video would render through the scene

--- scripts.js
+++ scripts.js
@@ -35,5 +35,17 @@ window.addEventListener('load', () => {
 		div.src = "./clouds/cloud0" + (Math.floor(Math.random() * 5) + 1) + ".png";
 		div.style.top = (Math.floor(Math.random() * 100) + 1) + "vh";
 		document.getElementById("clouds").appendChild(div);
-    }
+	}
+	const canvas = document.querySelector("canvas");
+	const ctx = canvas.getContext("2d");
+	const video = document.querySelector("video");
+
+	video.addEventListener('play', () => {
+		function step() {
+			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+			requestAnimationFrame(step);
+		}
+		requestAnimationFrame(step);
+	})
 });
+
