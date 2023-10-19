5e0963ecbc6b08e13dfd02d4398159ce3c11409f
ihunter <ijh.hunter@gmail.com>
2023-10-19 01:08:10 -0400

    clean up code

--- scripts.js
+++ scripts.js
@@ -1,69 +1,10 @@
 window.addEventListener("load", () => {
-	var button = document.getElementById("start");
+	const syncButton = document.getElementById("syncButton");
 
-	button.onclick = function () {
-		var d = new Date();
-		console.log(d.getSeconds());
-		console.log(d.getHours());
-		console.log(d.getMinutes());
-		console.log(d.getMilliseconds());
-		totalSeconds =
-			d.getSeconds() +
-			d.getHours() * 60 * 60 +
-			d.getMinutes() * 60 +
-			d.getMilliseconds() / 1000;
-		console.log(totalSeconds);
-		document
-			.getElementById("skyGradient")
-			.children[0].children[0].setAttribute("begin", "-" + totalSeconds + "s");
-		document
-			.getElementById("skyGradient")
-			.children[0].children[1].setAttribute("begin", "-" + totalSeconds + "s");
-		document
-			.getElementById("skyGradient")
-			.children[1].children[0].setAttribute("begin", "-" + totalSeconds + "s");
-
-		document
-			.getElementById("skyGradient")
-			.children[0].children[0].setAttribute("dur", "86400s");
-		document
-			.getElementById("skyGradient")
-			.children[0].children[1].setAttribute("dur", "86400s");
-		document
-			.getElementById("skyGradient")
-			.children[1].children[0].setAttribute("dur", "86400s");
-
-		document.getElementById("skyGradient").innerHTML += ""; //reload the svgs
-
-		document.getElementsByClassName("sun")[0].style.animationDuration =
-			"86400s";
-		document.getElementsByClassName("moon")[0].style.animationDuration =
-			"86400s";
-		document.getElementsByClassName("sun")[0].style.animationDelay =
-			"-" + totalSeconds + "s";
-		document.getElementsByClassName("moon")[0].style.animationDelay =
-			"-" + totalSeconds + "s";
-	};
+	syncButton.addEventListener("click", syncAnimationToLocalTime);
 
 	for (var i = 0; i < 25; i++) {
-		var cloudSpeed = Math.floor(Math.random() * 30) + 1;
-		console.log(cloudSpeed);
-		var div = document.createElement("img");
-		div.style.animationDuration = 15 * (1 + cloudSpeed / 10) + "s";
-		div.style.animationDelay =
-			"-" +
-			((Math.floor(Math.random() * 100) + 1) / 100) *
-			(15 * (1 + cloudSpeed / 10)) +
-			"s";
-		div.style.width = 30 * (cloudSpeed / 60) + "%";
-		// div.style.height = "auto";
-		// div.style.transform = "scale(" + (Math.floor(Math.random() * 100) + 0) + ")";
-		div.style.opacity = cloudSpeed / 20;
-		div.className = "cloud";
-		div.src =
-			"media/clouds/cloud0" + (Math.floor(Math.random() * 5) + 1) + ".png";
-		div.style.top = Math.floor(Math.random() * 70) + 1 + "vh";
-		document.getElementById("clouds").appendChild(div);
+		createCloud()
 	}
 
 	const canvas = document.querySelector("canvas");
@@ -78,24 +19,73 @@ window.addEventListener("load", () => {
 		requestAnimationFrame(step);
 	});
 
-	//Clock functionality
-	let clockUpdate = () => {
-		const time = document.getElementById("time");
-		let d = new Date();
-
-		// let currentHour = d.getHours();
-		// let hourDisplay = currentHour % 12 == 0 ? 12 : currentHour % 12;
-		// let minuteDisplay = String(d.getMinutes()).padStart(2, "0");
-		let milliseconds = d.getMilliseconds();
-
-		const localTime = d.toLocaleTimeString([], { hour: "numeric", minute: "numeric" })
-
-		time.textContent =
-			milliseconds > 500
-				? `${localTime.split(":").join(' ')}`
-				: `${localTime}`;
-	};
-
-	clockUpdate();
-	setInterval(clockUpdate, 500);
+	updateClock();
+	setInterval(updateClock, 500);
 });
+
+function syncAnimationToLocalTime() {
+	const d = new Date();
+
+	const secondsPerDay = 24 * 60 * 60;
+
+	const totalSeconds =
+		d.getSeconds() +
+		d.getHours() * 60 * 60 +
+		d.getMinutes() * 60 +
+		d.getMilliseconds() / 1000;
+
+	const skyGradient = document.getElementById("skyGradient");
+
+	skyGradient.children[0].children[0].setAttribute("begin", `-${totalSeconds}s`);
+	skyGradient.children[0].children[1].setAttribute("begin", `-${totalSeconds}s`);
+	skyGradient.children[1].children[0].setAttribute("begin", `-${totalSeconds}s`);
+
+	skyGradient.children[0].children[0].setAttribute("dur", `${secondsPerDay}s`);
+	skyGradient.children[0].children[1].setAttribute("dur", `${secondsPerDay}s`);
+	skyGradient.children[1].children[0].setAttribute("dur", `${secondsPerDay}s`);
+
+	skyGradient.innerHTML += ""; // reload the svgs
+
+	const sun = document.getElementById("sun");
+	const moon = document.getElementById("moon");
+
+	sun.style.animationDuration = `${secondsPerDay}s`;
+	moon.style.animationDuration = `${secondsPerDay}s`;
+	sun.style.animationDelay = `-${totalSeconds}s`;
+	moon.style.animationDelay = `-${totalSeconds}s`;
+}
+
+function updateClock() {
+	const time = document.getElementById("time");
+	const d = new Date();
+	const milliseconds = d.getMilliseconds();
+
+	const localTime = d.toLocaleTimeString([], { hour: "numeric", minute: "numeric" })
+
+	time.textContent = milliseconds > 500 ? `${localTime.split(":").join(' ')}` : `${localTime}`;
+}
+
+function createCloud() {
+	const cloudSpeed = getRandomRange(1, 30);
+
+	const cloudImg = document.createElement("img");
+
+	const cloudAnimationDuration = 15 * (1 + cloudSpeed / 10)
+	const cloudAnimationDelay = (getRandomRange(1, 100) / 100) * (15 * (1 + cloudSpeed / 10))
+
+	cloudImg.style.animationDuration = `${cloudAnimationDuration}s`;
+	cloudImg.style.animationDelay = `-${cloudAnimationDelay}s`;
+
+	cloudImg.style.width = 30 * (cloudSpeed / 60) + "%";
+
+	cloudImg.style.opacity = cloudSpeed / 20;
+	cloudImg.className = "cloud";
+	cloudImg.src = `media/clouds/cloud0${getRandomRange(1, 5)}.png`;
+	cloudImg.style.top = `${getRandomRange(1, 70)}vh`;
+
+	document.getElementById("clouds").appendChild(cloudImg);
+}
+
+function getRandomRange(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
