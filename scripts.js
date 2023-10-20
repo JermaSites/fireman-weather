01a4bdd0a97a166c83d11eeaffaaf58ef4ea142d
ihunter <ijh.hunter@gmail.com>
2023-10-19 23:29:59 -0400

    weather api

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
 	const syncButton = document.getElementById("syncButton");
 
 	syncButton.addEventListener("click", syncAnimationToLocalTime);
 
+	// const weatherData = await getWeather();
+
 	for (var i = 0; i < 25; i++) {
-		createCloud()
+		cloudFactory(weatherData);
 	}
 
 	const canvas = document.querySelector("canvas");
@@ -61,32 +63,90 @@ function updateClock() {
 	const d = new Date();
 	const milliseconds = d.getMilliseconds();
 
-	const localTime = d.toLocaleTimeString([], { hour: "numeric", minute: "numeric" })
+	const localTime = d.toLocaleTimeString([], { hour: "numeric", minute: "numeric" });
 
 	time.textContent = milliseconds > 500 ? `${localTime.split(":").join(' ')}` : `${localTime}`;
 }
 
-function createCloud() {
+// function createCloud() {
+// 	const cloudImg = document.createElement("img");
+
+// 	const cloudSpeed = getRandomRange(1, 30);
+// 	const cloudAnimationDuration = 15 * (1 + cloudSpeed / 10)
+// 	const cloudAnimationDelay = (getRandomRange(1, 100) / 100) * (15 * (1 + cloudSpeed / 10))
+
+// 	cloudImg.style.animationDuration = `${cloudAnimationDuration}s`;
+// 	cloudImg.style.animationDelay = `-${cloudAnimationDelay}s`;
+
+// 	cloudImg.style.width = 30 * (cloudSpeed / 60) + "%";
+
+// 	cloudImg.style.opacity = cloudSpeed / 20;
+// 	cloudImg.className = "cloud";
+// 	cloudImg.src = `media/clouds/cloud0${getRandomRange(1, 5)}.png`;
+// 	cloudImg.style.top = `${getRandomRange(1, 70)}vh`;
+
+// 	document.getElementById("clouds").appendChild(cloudImg);
+// }
+
+class Cloud {
+	imgEl = document.createElement("img");
+	width;
+	speed;
+	opacity;
+	src;
+	top;
+	duration;
+	delay;
+
+	constructor(src, speed, width, opacity, top, duration, delay) {
+		this.src = src;
+		this.speed = speed;
+		this.width = width;
+		this.opacity = opacity;
+		this.top = top;
+		this.duration = duration;
+		this.delay = delay;
+
+		this.imgEl.src = this.src;
+		this.imgEl.className = "cloud";
+		this.imgEl.style.animationDuration = this.duration;
+		this.imgEl.style.animationDelay = this.delay;
+		this.imgEl.style.width = this.width;
+		this.imgEl.style.opacity = this.opacity;
+		this.imgEl.style.top = this.top;
+	}
+}
+
+function cloudFactory() {
+	const cloudImgSrc = `media/clouds/cloud0${getRandomRange(1, 5)}.png`;
 	const cloudSpeed = getRandomRange(1, 30);
+	const cloudWidth = 30 * (cloudSpeed / 60) + "%";
+	const cloudOpacity = cloudSpeed / 20;
+	const cloudTopOffset = `${getRandomRange(1, 70)}vh`;
+	const cloudAnimationDuration = `${15 * (1 + cloudSpeed / 10)}s`;
+	const cloudAnimationDelay = `-${(getRandomRange(1, 100) / 100) * (15 * (1 + cloudSpeed / 10))}s`;
 
-	const cloudImg = document.createElement("img");
-
-	const cloudAnimationDuration = 15 * (1 + cloudSpeed / 10)
-	const cloudAnimationDelay = (getRandomRange(1, 100) / 100) * (15 * (1 + cloudSpeed / 10))
-
-	cloudImg.style.animationDuration = `${cloudAnimationDuration}s`;
-	cloudImg.style.animationDelay = `-${cloudAnimationDelay}s`;
-
-	cloudImg.style.width = 30 * (cloudSpeed / 60) + "%";
-
-	cloudImg.style.opacity = cloudSpeed / 20;
-	cloudImg.className = "cloud";
-	cloudImg.src = `media/clouds/cloud0${getRandomRange(1, 5)}.png`;
-	cloudImg.style.top = `${getRandomRange(1, 70)}vh`;
-
-	document.getElementById("clouds").appendChild(cloudImg);
+	const cloud = new Cloud(cloudImgSrc, cloudSpeed, cloudWidth, cloudOpacity, cloudTopOffset, cloudAnimationDuration, cloudAnimationDelay);
+	document.getElementById("clouds").appendChild(cloud.imgEl);
 }
 
 function getRandomRange(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
+}
+
+async function getCoords() {
+	const position = await new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+
+	const { latitude, longitude } = position.coords;
+
+	return { latitude, longitude };
+}
+
+async function getWeather() {
+	const { latitude, longitude } = await getCoords();
+
+	const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation,rain,showers,snowfall,windspeed_10m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York`);
+	return await response.json();
 }
